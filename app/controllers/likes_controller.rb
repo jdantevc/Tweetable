1d7eebaf6aef5818171f6b23fbdb4d0f1258cdb3
Dante Vasquez <jdantevc@gmail.com>
2023-11-28 15:35:59 -0500

    ADD: validations to tables

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,5 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_like, only: %i[ show edit update destroy ]
 
   # GET /likes
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,4 +6,15 @@ class Tweet < ApplicationRecord
    #Associations
    has_many :likes
    has_many :liked_by_users, through: :likes, source: :user
+    # Validations
+  validates :body, presence: true, length: { maximum: 140 }
+  validate :valid_replied_to_id, unless: -> { replied_to_id.nil? }
+
+  private
+
+  def valid_replied_to_id
+    unless Tweet.exists?(replied_to_id)
+      errors.add(:replied_to_id, "must be a valid Tweet id")
+    end
+  end
 end
